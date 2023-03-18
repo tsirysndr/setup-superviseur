dd6ed23297c7eeb13f1751fc673596df935592f0
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-03-19 00:07:26 +0300

    fix import error
    fix import error

--- src/action.ts
+++ src/action.ts
@@ -1,6 +1,6 @@
 import { tmpdir } from "node:os";
 import * as action from "@actions/core";
-import setup from "./setup";
+import setup from "./setup.js";
 
 if (!process.env.RUNNER_TEMP) {
   process.env.RUNNER_TEMP = tmpdir();
